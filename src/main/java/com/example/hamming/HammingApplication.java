1a14da4c53bc76f27dded814193ba2a20d398986
Hao Lin <130253423+pHaoq@users.noreply.github.com>
2023-06-13 11:27:03 +0200

    first commit

---
+++ src/main/java/com/example/hamming/HammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hamming/HammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hamming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
